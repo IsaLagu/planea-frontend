6440f092c511e43940a40c1cfa9ce28030a03913
Isa Afonso <126961475+IsaLagu@users.noreply.github.com>
2024-09-05 17:43:26 +0100

    wip signup

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,16 @@
-export const Input = ({ type = "text", id, className, placeholder }) => {
+import { forwardRef } from "react";
+
+export const Input = forwardRef(({ type = "text", id, className, placeholder, ...rest }, ref) => {
   return (
     <input
       type={type}
       id={id}
+      ref={ref}
       className={`shadow-sm bg-gray-50 border border-gray-300 text-darkGrey text-sm rounded-lg focus:outline-none focus:border-primary block w-full p-2.5${
         className ? " " + className : ""
       }`}
       placeholder={placeholder}
+      {...rest}
     />
   );
-};
+});
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,6 +3,10 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 import { signUpSchema } from "../hooks/schemaValidation";
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../context/UserContext";
+import usePost from "../hooks/usePost";
+import { useEffect } from "react";
 
 export const SignUp = () => {
   const {
@@ -13,9 +17,20 @@ export const SignUp = () => {
     resolver: yupResolver(signUpSchema),
   });
 
-  const onSubmit = (data) => {
-    console.log(data); // Aquí manejas el envío del formulario
+  const { error, executePost, data } = usePost("/auth/signup");
+  const { setToken } = useUser();
+  const navigate = useNavigate();
+
+  const onSubmit = (formData) => {
+    console.log(formData);
   };
+
+  useEffect(() => {
+    if (data) {
+      setToken(data.token);
+      navigate("/");
+    }
+  }, [data, setToken, navigate]);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="max-w-sm mx-auto">
@@ -23,7 +38,7 @@ export const SignUp = () => {
         <label htmlFor="email" className="block mb-2 text-sm font-medium text-darkGrey">
           Tu email
         </label>
-        <Input {...register("email")} type="email" id="email" className="" placeholder="nombre@planea.com" />
+        <Input {...register("email")} type="email" id="email" placeholder="nombre@planea.com" />
         {errors.email && <p className="text-red-500 text-sm">{errors.email.message}</p>}
       </div>
 
@@ -31,7 +46,7 @@ export const SignUp = () => {
         <label htmlFor="password" className="block mb-2 text-sm font-medium text-darkGrey">
           Tu contraseña
         </label>
-        <Input {...register("password")} type="password" id="password" className="" placeholder="" />
+        <Input {...register("password")} type="password" id="password" />
         {errors.password && <p className="text-red-500 text-sm">{errors.password.message}</p>}
       </div>
 
@@ -39,7 +54,7 @@ export const SignUp = () => {
         <label htmlFor="repeat-password" className="block mb-2 text-sm font-medium text-darkGrey">
           Repite tu contraseña
         </label>
-        <Input {...register("repeatPassword")} type="password" id="repeat-password" className="" placeholder="" />
+        <Input {...register("repeatPassword")} type="password" id="repeat-password" />
         {errors.repeatPassword && <p className="text-red-500 text-sm">{errors.repeatPassword.message}</p>}
       </div>
 
@@ -60,7 +75,7 @@ export const SignUp = () => {
         </label>
       </div>
 
-      <Button onClick={onclick} text="Crea una nueva cuenta" className="" />
+      <Button>Crear cuenta</Button>
     </form>
   );
 };
