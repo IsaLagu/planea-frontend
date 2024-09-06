5ceec2a4c7bbbf6dbef163fb94283382fdc704c7
Isa Afonso <126961475+IsaLagu@users.noreply.github.com>
2024-09-06 21:24:58 +0100

    Feature/plan 5 navbar (#8)
    * add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
       colors: {
         primary: "#7B03D9",
         lightViolet: "#9623EF",
+        lightPink: "#E6C7FF",
         grey: "#758CA3",
         darkGrey: "#525153",
         cream: "#F9F6F6",
