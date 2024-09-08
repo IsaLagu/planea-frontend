090be8d0a0312427b4432fb94fc2de1760eb4f4f
Isa Afonso <126961475+IsaLagu@users.noreply.github.com>
2024-09-08 09:43:02 +0200

    add time inputs

--- src/pages/events/CreateEvent.jsx
+++ src/pages/events/CreateEvent.jsx
@@ -27,8 +27,9 @@ export const CreateEvent = () => {
 
   return (
     <>
+      <h1 className="text-primary text-center text-3xl font-bold mb-9">Crea tu evento</h1>
       <form noValidate onSubmit={handleSubmit(onSubmit)} className="max-w-lg mx-auto">
-        <div className="mb-5">
+        <div className="mb-9">
           <label htmlFor="name" className="block mb-2 font-medium tracking-wide text-darkGrey">
             Nombre del evento
           </label>
@@ -36,7 +37,7 @@ export const CreateEvent = () => {
           {errors.name && <p className="text-red-500 text-sm">{errors.name.message}</p>}
         </div>
 
-        <div className="mb-5">
+        <div className="mb-9">
           <label htmlFor="location" className="block mb-2 font-medium tracking-wide text-darkGrey">
             Ubicación
           </label>
@@ -50,9 +51,9 @@ export const CreateEvent = () => {
           />
         </div>
 
-        <div className="mb-5">
+        <div className="mb-9">
           <label className="block mb-2 font-medium tracking-wide text-darkGrey" htmlFor="event-date">
-            Fecha del evento
+            Fecha
           </label>
           <Datepicker
             id="event-date"
@@ -66,7 +67,73 @@ export const CreateEvent = () => {
           />
         </div>
 
-        <div className="mb-5">
+        <div className="block mb-2 font-medium tracking-wide text-darkGrey">Hora</div>
+        <div className="max-w-[16rem] grid grid-cols-2 gap-4 mb-9">
+          <div>
+            <label htmlFor="start-time" className="block mb-2 text-sm font-medium text-darkGrey">
+              Hora de inicio:
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 end-0 top-0 flex items-center pe-3.5 pointer-events-none">
+                <svg
+                  className="w-4 h-4 text-grey"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <input
+                type="time"
+                id="start-time"
+                className="bg-gray-50 border leading-none border-gray-300 text-darkGrey text-sm rounded-lg focus:border-primary block w-full p-2.5"
+                min="09:00"
+                max="18:00"
+                value="00:00"
+                required
+              />
+            </div>
+          </div>
+          <div>
+            <label htmlFor="end-time" className="block mb-2 text-sm font-medium text-darkGrey">
+              Hora de fin:
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 end-0 top-0 flex items-center pe-3.5 pointer-events-none">
+                <svg
+                  className="w-4 h-4 text-grey"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <input
+                type="time"
+                id="end-time"
+                className="bg-gray-50 border leading-none border-gray-300 text-darkGrey text-sm rounded-lg focus:border-primary block w-full p-2.5"
+                min="09:00"
+                max="18:00"
+                value="00:00"
+                required
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="mb-9">
           <label className="block mb-2 font-medium tracking-wide text-darkGrey" htmlFor="event-img">
             Imagen del evento
           </label>
@@ -74,16 +141,16 @@ export const CreateEvent = () => {
           <div className="mt-1 text-sm text-gray-500">SVG, PNG, JPG or GIF (MAX. 800x400px).</div>
         </div>
 
-        <div className="mb-5">
+        <div className="mb-9">
           <label htmlFor="price" className="block mb-2 font-medium tracking-wide text-darkGrey">
             Precio de las entradas
           </label>
           <Input type="text" id="price" placeholder="0,00€" />
         </div>
 
-        <div className="mb-5">
+        <div className="mb-9">
           <label htmlFor="capacity" className="block mb-2 font-medium tracking-wide text-darkGrey">
-            Capacidad del evento
+            Capacidad
           </label>
           <Input type="text" id="capacity" placeholder="nº de personas" />
         </div>
