e1a74804669b53e11b5e69318446f38e5dbc3d56
Isa Afonso <126961475+IsaLagu@users.noreply.github.com>
2024-09-09 23:42:17 +0200

    refactor schemas

--- src/pages/events/createEventSchema.js
+++ src/pages/events/createEventSchema.js
@@ -5,16 +5,12 @@ export const createEventSchema = yup.object().shape({
   description: yup.string().max(50, "No puedes exceder los 50 caracteres"),
   cityId: yup.string().required("La ciudad es obligatoria"),
   location: yup.string().required("La ubicación es obligatoria").max(50, "No puedes exceder los 50 caracteres"),
-  date: yup
-    .object()
-    .shape({
-      startDate: yup.date().required("La fecha de inicio es obligatoria"),
-      endDate: yup.date().required("La fecha de fin es obligatoria"),
-    })
-    .required("La fecha es obligatoria"),
-  categoryIds: yup.array().of(yup.string()).required("La categoría es obligatoria"),
+  categoryIds: yup.array().min(1, "Debes seleccionar al menos una categoría").required("La categoría es obligatoria"),
   image: yup
     .mixed()
+    .test("required", "Subir una imagen es obligatorio", (value) => {
+      return value && value.length > 0;
+    })
     .test("fileSize", "El archivo es demasiado grande", (value) => {
       return value && value[0] && value[0].size <= 5 * 1024 * 1024;
     })
@@ -23,9 +19,18 @@ export const createEventSchema = yup.object().shape({
     })
     .nullable()
     .notRequired(),
+
   price: yup
     .string()
     .notRequired()
-    .matches(/^\d+(\.\d{1,2})?$/, "El precio debe ser un número válido"),
-  capacity: yup.string().notRequired().matches(/^\d+$/, "La capacidad debe ser un número válido"),
+    .test(
+      "is-valid-price",
+      "El precio debe ser un número válido",
+      (value) => !value || /^\d+(\.\d{1,2})?$/.test(value)
+    ),
+
+  capacity: yup
+    .string()
+    .notRequired()
+    .test("is-valid-capacity", "La capacidad debe ser un número válido", (value) => !value || /^\d+$/.test(value)),
 });
