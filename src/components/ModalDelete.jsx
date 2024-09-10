78158b9cb6bab0b30b04268c6098e9197c7952d2
Isa Afonso <126961475+IsaLagu@users.noreply.github.com>
2024-09-10 21:15:16 +0200

    add modal

--- src/components/ModalDelete.jsx
+++ src/components/ModalDelete.jsx
@@ -1,16 +1,12 @@
-export const ModalDelete = () => {
+export const ModalDelete = ({ onClose }) => {
   return (
-    <div
-      id="popup-modal"
-      tabindex="-1"
-      className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
-    >
+    <div className="flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
       <div className="relative p-4 w-full max-w-md max-h-full">
         <div className="relative bg-white rounded-lg shadow">
           <button
             type="button"
             className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
-            data-modal-hide="popup-modal"
+            onClick={onClose}
           >
             <svg
               className="w-3 h-3"
@@ -47,14 +43,14 @@ export const ModalDelete = () => {
             </svg>
             <h3 className="mb-5 text-lg font-normal text-gray-500">¿Estás seguro de que quieres borrar este evento?</h3>
             <button
-              data-modal-hide="popup-modal"
+              onClick={onClose}
               type="button"
               className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
             >
               Aceptar
             </button>
             <button
-              data-modal-hide="popup-modal"
+              onClick={onClose}
               type="button"
               className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100"
             >
