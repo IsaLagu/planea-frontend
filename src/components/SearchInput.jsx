02e476c59cf1e236527fe0f54891549d5ae3da75
Isa Afonso <126961475+IsaLagu@users.noreply.github.com>
2024-09-12 13:08:28 +0200

    add cities API integration

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,60 +1,90 @@
-export const SearchInput = () => {
+import { useState } from "react";
+import { Spinner } from "./Spinner";
+
+export const SearchInput = ({ cities, citiesLoading, citiesError }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
-    <form class="max-w-lg mx-auto mb-10">
-      <div class="flex">
-        <label for="search-dropdown" class="mb-2 text-sm font-medium text-darkGrey sr-only">
+    <form className="max-w-lg mx-auto mb-10">
+      <div className="flex">
+        <label htmlFor="search-dropdown" className="mb-2 text-sm font-medium text-darkGrey sr-only">
           Your Email
         </label>
         <button
           id="dropdown-button"
-          data-dropdown-toggle="dropdown"
-          class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-white bg-primary border-gray-300 rounded-s-lg hover:bg-lightViolet focus:ring-4 focus:outline-none focus:ring-primary"
+          onClick={toggleDropdown}
+          className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-white bg-primary border-gray-300 rounded-s-lg hover:bg-lightViolet focus:ring-4 focus:outline-none focus:ring-primary"
           type="button"
         >
-          Ciudades{" "}
+          Ciudades de Tenerife{" "}
           <svg
-            class="w-2.5 h-2.5 ms-2.5"
+            className="w-2.5 h-2.5 ms-2.5"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 10 6"
           >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m1 1 4 4 4-4"
-            />
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
           </svg>
         </button>
-        <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
-          <ul class="py-2 text-sm text-gray-700" aria-labelledby="dropdown-button">
-            <li></li>
-          </ul>
-        </div>
-        <div class="relative w-full">
+        {isDropdownOpen && (
+          <div
+            id="dropdown"
+            className="z-10 p-2 mt-4 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 absolute"
+          >
+            <ul className="py-2 text-sm text-gray-700" aria-labelledby="dropdown-button">
+              {citiesLoading && <Spinner />}
+              {citiesError && <p>Error: {citiesError.message}</p>}
+              {
+                !citiesLoading && !citiesError && cities && cities.length > 0
+                  ? cities.map((city) => (
+                      <li key={city.id}>
+                        <button
+                          className="hover:bg-lightViolet checked:bg-primary active:bg-primary p-1 mb-0.5 rounded-md hover:text-white checked:text-white w-full text-left"
+                          value={city.id}
+                        >
+                          {city.name}
+                        </button>
+                      </li>
+                    ))
+                  : !citiesLoading &&
+                    !citiesError && <p className="text-2xl text-darkGrey font-medium">No hay ciudades disponibles</p> // Mensaje si no hay ciudades
+              }
+            </ul>
+          </div>
+        )}
+        <div className="relative w-full">
           <input
             type="search"
             id="search-dropdown"
-            class="block p-2.5 w-full z-20 text-sm text-gdarkGrey bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:outline-none focus:border-2 focus:border-primary"
+            className="block p-2.5 w-full z-20 text-sm text-gdarkGrey bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:outline-none focus:border-2 focus:border-primary"
             placeholder="Busca eventos"
             required
           />
           <button
             type="submit"
-            class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-primary rounded-e-lg border border-primary hover:bg-lightViolet focus:ring-4 focus:outline-none focus:ring-primary"
+            className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-primary rounded-e-lg border border-primary hover:bg-lightViolet focus:ring-4 focus:outline-none focus:ring-primary"
           >
-            <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <svg
+              className="w-4 h-4"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
               />
             </svg>
-            <span class="sr-only">Buscar</span>
+            <span className="sr-only">Buscar</span>
           </button>
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,19 +4,23 @@ import { Spinner } from "../components/Spinner";
 import useGet from "../hooks/useGet";
 
 export const Home = () => {
-  const { data: events, loading, error } = useGet(`/api/events`);
+  const { data: events, loading: eventsLoading, error: eventsError } = useGet(`/api/events`);
+  const { data: cities, loading: citiesLoading, error: citiesError } = useGet(`/api/cities`);
 
   return (
     <div>
-      <SearchInput />
+      <p className="text-center text-3xl font-bold text-primary mb-10">Encuentra lo que te gusta</p>
+
+      <SearchInput cities={cities || []} />
+
       <div className="max-w-screen-xl flex flex-wrap mx-auto justify-center gap-x-8 gap-y-10">
         {events?.length ? (
           events.map((event) => <EventCard key={event.id} event={event} />)
         ) : (
-          <>{!loading && <p className="text-2xl text-darkGrey font-medium">No hay eventos disponibles</p>}</>
+          <>{!eventsLoading && <p className="text-2xl text-darkGrey font-medium">No hay eventos disponibles</p>}</>
         )}
-        {loading && <Spinner />}
-        {error && <p>Error: {error.message}</p>}
+        {eventsLoading && <Spinner />}
+        {eventsError && <p>Error: {eventsError.message}</p>}
       </div>
     </div>
   );
